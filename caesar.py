709393ecb62cd882b1d017af969f48b57131eae5
ulgoon <ulgoon89@gmail.com>
2023-05-02 14:08:38 +0900

    fix: Do caesar also with Uppercase
    Worked only lowercase.
    So, make plaintext to lower and translate by the table.

--- caesar.py
+++ caesar.py
@@ -5,7 +5,7 @@ def caesar(plaintext: str, shift: int) -> str:
     alphabet = string.ascii_lowercase
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     table = str.maketrans(alphabet, shifted_alphabet)
-    return plaintext.translate(table)
+    return plaintext.lower().translate(table)
 
 
 if __name__ == '__main__':
